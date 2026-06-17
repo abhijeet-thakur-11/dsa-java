b66a69f9da6477971e1920f6c35f768429132033
abhijeet <abhijeetthakur.nov2006@gmail.com>
2026-06-17 18:11:56 +0530

    Auto-update from VS Code

--- Basics.java
+++ Basics.java
@@ -130,7 +130,31 @@ public class Basics {
         //else
             //System.out.println("I don't know what will happen");
 
+        
+        //Switch 
+        //int day = 1;
+        //switch(day){
+          //  case 1 : System.out.println("Today is monday");
+            //break;
+         //   case 2 : System.out.println("Today is tuesday");
+          //  break;
+            //case 3 : System.out.println("Today is wednesday");
+            //break;
+            //case 4 : System.out.println("Today is thursday");
+            //break;  
+            //case 5 : System.out.println("Today is friday");
+            //break;
+            //case 6 : System.out.println("Today is saturday");
+            //break;
+            //case 7 : System.out.println("Today is sunday");
+            //break;
+               // default : System.out.println("Invalid day");  
+            
 
+            //Loops 
+
+            
+        }
 
 
         
