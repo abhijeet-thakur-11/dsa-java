3c25ff8daab364ae25d398dd0d350c749ee53c48
abhijeet <abhijeetthakur.nov2006@gmail.com>
2026-06-18 23:48:08 +0530

    Auto-update from VS Code

--- dsa.java
+++ dsa.java
@@ -1,21 +1,24 @@
 public class dsa {
 
-    public static void printnum(int n){
-        if (n==0){
-            return;
+    public static void calcfib(int a , int b , int n){
+        if(n==0){
+            return ;
         }
-        System.out.println(n);
-        printnum(n-1);
-    }
+        int c = a+b ;
+        System.out.println(c);
+        calcfib(b,c,n-1);
+    }    
     public static void main(String[] args) {
+        int a = 0;
+        int b =1;
+        int n= 15;
+        System.out.println(a);
+        System.out.println(b);
+        calcfib(a,b,n-2);
+       
         // Recursion :- A function that calls itself
 
-        printnum(6);
-               
-
-
-
-
+    
     }
     
 }
