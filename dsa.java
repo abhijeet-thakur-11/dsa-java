e0819aac1405dd48d6955d7a5aa7011172aa74f1
abhijeet <abhijeetthakur.nov2006@gmail.com>
2026-06-19 21:28:07 +0530

    Day :- 3

--- dsa.java
+++ dsa.java
@@ -1,5 +1,6 @@
 public class dsa {
-
+    
+    // fibonacci series using recursion
     public static void calcfib(int a , int b , int n){
         if(n==0){
             return ;
@@ -7,18 +8,57 @@ public class dsa {
         int c = a+b ;
         System.out.println(c);
         calcfib(b,c,n-1);
-    }    
-    public static void main(String[] args) {
-        int a = 0;
-        int b =1;
-        int n= 15;
-        System.out.println(a);
-        System.out.println(b);
-        calcfib(a,b,n-2);
-       
-        // Recursion :- A function that calls itself
-
+    } 
     
+    //print x^n(stack height = n)
+    public static int calcpower(int x , int n){
+        if(n==0){
+            return 1;
+        }if (x==0){
+            return 1;
+        }
+        int xpowernm1 = calcpower(x , n-1);
+        int xpowern = x*xpowernm1;
+        return xpowern;
+
+    }
+
+    //Hanoi Tower
+    public static void hanoi(int n , String src , String helper , String dest ){
+        if (n==1){
+            System.out.println("Transfer disk " + n + " from " + src + " to " + dest);
+            return;
+        }
+        hanoi(n-1 , src , dest , helper );
+        System.out.println("Transfer disk " + n + " from " + src + " to " + dest);
+        hanoi(n-1 , helper , src , dest );
+
+    }
+
+    // reverse the string using recursion 
+    public static void revstr(String str , int ind){
+        if (ind == 0){
+            System.out.println(str.charAt(ind));
+            return;
+        }
+        System.out.println(str.charAt(ind));
+        revstr(str , ind-1);
+    }
+    
+    
+    public static void main(String[] args) {
+        
+        // Recursion :- A function that calls itself
+        int n = 3;
+        //hanoi(n , "s", "h" , "d");
+        String str = "Abhijeet thaur";
+
+        revstr(str,str.length()-1);
+
+
+
+
+
     }
     
 }
