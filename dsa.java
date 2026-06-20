20b48e76e8ef1ad7a4423a1e537f496bc93d40f9
abhijeet <abhijeetthakur.nov2006@gmail.com>
2026-06-20 14:06:03 +0530

    Auto-update from VS Code

--- dsa.java
+++ dsa.java
@@ -1,5 +1,6 @@
 public class dsa {
-    
+    /*
+
     // fibonacci series using recursion
     public static void calcfib(int a , int b , int n){
         if(n==0){
@@ -9,7 +10,7 @@ public class dsa {
         System.out.println(c);
         calcfib(b,c,n-1);
     } 
-    /*
+    */ /*
     //print x^n(stack height = n)
     public static int calcpower(int x , int n){
         if(n==0){
@@ -35,7 +36,7 @@ public class dsa {
         hanoi(n-1 , helper , src , dest );
 
     }
-*/
+*//*
     // reverse the string using recursion 
     public static void revstr(String str , int ind){
         if (ind == 0){
@@ -44,20 +45,96 @@ public class dsa {
         }
         System.out.println(str.charAt(ind));
         revstr(str , ind-1);
+    }*/
+/* 
+    // First and last occurence of a letter in the string 
+    public static int first = -1;
+    public static int last = -1;
+
+    public static void frstlst(String str, int idx , char element){
+        if(idx == str.length()){
+            System.out.println(first);
+            System.out.println(last);
+            return;
+
+        }
+        
+        char curchar = str.charAt(idx);
+        if(curchar == element){
+           if (first == -1){
+            first = idx ;
+           }else {
+            last = idx ;
+        }frstlst(str , idx+1 , element);
     }
-    
+    }
+    */
+/* 
+    //to check array is sorted or not 
+
+    public static boolean checksort(int arry[] , int idx ){
+        if(idx == arry.length - 1){
+            return true;
+        }
+        if(arry[idx]<arry[idx+1]){
+            return checksort(arry , idx+1);
+        }else{
+            return false;
+        }
+    }
+  */
+ /* 
+    //move all the x to the end of the string 
+    public static void moveallx(String str , int idx , int count ,String nwstr ){
+        
+        if(idx == str.length()){
+            for (int i = 0 ; i<count ; i++){
+                nwstr = nwstr +'x';
+
+            }
+            System.out.println(nwstr);
+            return;
+            
+        }
+
+        char curchar = str.charAt(idx);
+        if(curchar == 'x'){
+            count++;
+            moveallx(str , idx+1 ,count , nwstr);
+        }else{
+            nwstr = nwstr+curchar;
+            moveallx(str , idx+1, count , nwstr);
+        }
+
+    }*/
+   
+   //remove the duplicate from the string
+    public static boolean[] map = new boolean[26];
+    public static void removeduplicates(String str , int idx , boolean[] map , String nwstr){
+        if(idx == str.length()){
+            System.out.println(nwstr);
+            return;
+        }
+        char curchar = str.charAt(idx);
+        if(map[curchar - 'a' ] == true){
+            removeduplicates(str , idx +1 ,  map , nwstr);
+
+        }else{
+            nwstr+=curchar;
+            map[curchar-'a'] = true;
+            removeduplicates(str , idx+1 , map , nwstr);
+        }
+
+    }
+      
     
     public static void main(String[] args) {
         
         // Recursion :- A function that calls itself
-        int n = 3;
-        //hanoi(n , "s", "h" , "d");
-        String str = "Abhijeet thaur";
-
-        revstr(str,str.length()-1);
-
-
-
+        String str = "abaacbcdbdda";
+        removeduplicates(str , 0 , map ," ");
+        
+         
 
 
     }
