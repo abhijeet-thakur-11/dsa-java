50298eb0380b48c7d32ea383e26e88b074b85887
abhijeet <abhijeetthakur.nov2006@gmail.com>
2026-06-22 01:25:58 +0530

    Add N-Queens backtracking solution

---
+++ backtacking.java
@@ -0,0 +1,124 @@
+import java.util.*;
+
+public class backtacking {
+    // Backtacking
+
+    /*
+     * public static void printpermutations(String str , int idx , String perm){
+     * if(str.length() == 0 ){
+     * System.out.println(perm);
+     * return;
+     * }
+     * for (int i = 0; i<str.length() ; i++){
+     * char curchar = str.charAt(i);
+     * String nwstr = str.substring(0,i) + str.substring(i+1);
+     * printpermutations(nwstr , idx+1 , perm+curchar);
+     * }
+     * 
+     * }
+     */
+
+    // is the queen safe or not ( N Queens )
+    public static boolean isSafe(int row, int col, char[][] board) {
+        // horizontal
+        for (int j = 0; j < board.length; j++) {
+            if (board[row][j] == 'Q') {
+                return false;
+            }
+        }
+
+        // vertical
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+
+        // upper left
+        int r = row;
+        for (int c = col; r >= 0 && c >= 0; r--, c--) {
+            if (board[r][c] == 'Q') {
+                return false;
+            }
+        }
+
+        // upper right
+        r = row;
+        for (int c = col; c < board.length && r >= 0; r--, c++) {
+            if (board[r][c] == 'Q') {
+                return false;
+            }
+        }
+
+        // lower left
+        r = row;
+        for (int c = col; r < board.length && c >= 0; r++, c--) {
+            if (board[r][c] == 'Q') {
+                return false;
+            }
+        }
+
+        // lower right
+        for (int c = col, r2 = row; c < board.length && r2 < board.length; r2++, c++) {
+            if (board[r2][c] == 'Q') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void saveBoard(char[][] board, List<List<String>> allBoards) {
+        String row = "";
+        List<String> newBoard = new ArrayList<>();
+
+        for (int i = 0; i < board.length; i++) {
+            row = "";
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == 'Q')
+                    row += 'Q';
+                else
+                    row += '.';
+            }
+            newBoard.add(row);
+        
+        }
+        allBoards.add(newBoard);
+    }
+
+    public void helper(char[][] board, List<List<String>> allBoards, int col) {
+        if (col == board.length) {
+            saveBoard(board, allBoards);
+            return;
+        }
+
+        for (int row = 0; row < board.length; row++) {
+            if (isSafe(row, col, board)) {
+                board[row][col] = 'Q';
+                helper(board, allBoards, col + 1);
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> allBoards = new ArrayList<>();
+        char[][] board = new char[n][n];
+
+        helper(board, allBoards, 0);
+        return allBoards;
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        backtacking solver = new backtacking();
+        List<List<String>> solutions = solver.solveNQueens(n);
+
+        System.out.println("Solutions for " + n + "-Queens:");
+        for (List<String> board : solutions) {
+            for (String row : board) {
+                System.out.println(row);
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ dsa.java
@@ -0,0 +1,205 @@
+import java.util.HashSet;
+
+public class dsa {
+    /*
+     * 
+     * // fibonacci series using recursion
+     * public static void calcfib(int a , int b , int n){
+     * if(n==0){
+     * return ;
+     * }
+     * int c = a+b ;
+     * System.out.println(c);
+     * calcfib(b,c,n-1);
+     * }
+     */
+    /*
+     * //print x^n(stack height = n)
+     * public static int calcpower(int x , int n){
+     * if(n==0){
+     * return 1;
+     * }if (x==0){
+     * return 1;
+     * }
+     * int xpowernm1 = calcpower(x , n-1);
+     * int xpowern = x*xpowernm1;
+     * return xpowern;
+     * 
+     * }
+     */
+    /*
+     * //Hanoi Tower
+     * public static void hanoi(int n , String src , String helper , String dest ){
+     * if (n==1){
+     * System.out.println("Transfer disk " + n + " from " + src + " to " + dest);
+     * return;
+     * }
+     * hanoi(n-1 , src , dest , helper );
+     * System.out.println("Transfer disk " + n + " from " + src + " to " + dest);
+     * hanoi(n-1 , helper , src , dest );
+     * 
+     * }
+     *//*
+        * // reverse the string using recursion
+        * public static void revstr(String str , int ind){
+        * if (ind == 0){
+        * System.out.println(str.charAt(ind));
+        * return;
+        * }
+        * System.out.println(str.charAt(ind));
+        * revstr(str , ind-1);
+        * }
+        */
+    /*
+     * // First and last occurence of a letter in the string
+     * public static int first = -1;
+     * public static int last = -1;
+     * 
+     * public static void frstlst(String str, int idx , char element){
+     * if(idx == str.length()){
+     * System.out.println(first);
+     * System.out.println(last);
+     * return;
+     * 
+     * }
+     * 
+     * char curchar = str.charAt(idx);
+     * if(curchar == element){
+     * if (first == -1){
+     * first = idx ;
+     * }else {
+     * last = idx ;
+     * }frstlst(str , idx+1 , element);
+     * }
+     * }
+     */
+    /*
+     * //to check array is sorted or not
+     * 
+     * public static boolean checksort(int arry[] , int idx ){
+     * if(idx == arry.length - 1){
+     * return true;
+     * }
+     * if(arry[idx]<arry[idx+1]){
+     * return checksort(arry , idx+1);
+     * }else{
+     * return false;
+     * }
+     * }
+     */
+    /*
+     * //move all the x to the end of the string
+     * public static void moveallx(String str , int idx , int count ,String nwstr ){
+     * 
+     * if(idx == str.length()){
+     * for (int i = 0 ; i<count ; i++){
+     * nwstr = nwstr +'x';
+     * 
+     * }
+     * System.out.println(nwstr);
+     * return;
+     * 
+     * }
+     * 
+     * char curchar = str.charAt(idx);
+     * if(curchar == 'x'){
+     * count++;
+     * moveallx(str , idx+1 ,count , nwstr);
+     * }else{
+     * nwstr = nwstr+curchar;
+     * moveallx(str , idx+1, count , nwstr);
+     * }
+     * 
+     * }
+     */
+    /*
+     * //remove the duplicate from the string
+     * public static boolean[] map = new boolean[26];
+     * public static void removeduplicates(String str , int idx , boolean[] map ,
+     * String nwstr){
+     * if(idx == str.length()){
+     * System.out.println(nwstr);
+     * return;
+     * }
+     * char curchar = str.charAt(idx);
+     * if(map[curchar - 'a' ] == true){
+     * removeduplicates(str , idx +1 , map , nwstr);
+     * 
+     * }else{
+     * nwstr+=curchar;
+     * map[curchar-'a'] = true;
+     * removeduplicates(str , idx+1 , map , nwstr);
+     * }
+     * 
+     * }
+     */
+    /*
+     * // subsequesnce of the string
+     * public static void subsequence(String str , int idx , String nwstr){
+     * if(idx==str.length()){
+     * System.out.println(nwstr);
+     * return;
+     * }
+     * char curchar = str.charAt(idx);
+     * //to be
+     * subsequence(str , idx+1 , nwstr+curchar);
+     * //not to be
+     * subsequence(str , idx+1 , nwstr);
+     * }
+     */
+    /*
+     * // unique subsequesnce of the string
+     * public static void subsequence(String str , int idx , String nwstr ,
+     * HashSet<String> set){
+     * if(idx==str.length()){
+     * if(set.contains(nwstr)){
+     * return;
+     * } else{
+     * System.out.println(nwstr);
+     * set.add(nwstr);
+     * return;
+     * }
+     * char curchar = str.charAt(idx);
+     * //to be
+     * subsequence(str , idx+1 , nwstr+curchar , set);
+     * //not to be
+     * subsequence(str , idx+1 , nwstr , set);
+     * }
+     */
+    /*
+     * //print keypad combination
+     * public static String[] keypad = {".;" , "abc" , "def" , "ghi" , "jkl" , "mno"
+     * , "pqrs" , "tuv" , "wxyz"};
+     * public static void printcomb(String str , int idx , String comb){
+     * if(idx == str.length()){
+     * System.out.println(comb);
+     * return;
+     * }
+     * char curchar = str.charAt(idx);
+     * String mapping = keypad[curchar - '0'];
+     * for ( int i = 0 ; i<mapping.length() ; i++){
+     * printcomb(str , idx+1 , comb+mapping.charAt(i));
+     * }
+     * }
+     */
+    /*
+     * 
+     */
+
+    // print all the solution where queen are safe
+    public static
+
+    //public static void main(String[] args) {
+    public List<List<String>> solveNQueens(int n ){ 
+
+    }
+
+        // Recursion :- A function that calls itself
+        // HashSet<String> set = new HashSet<>(); fpr uniques subsequence of the sting
+
+        // Backtracking :- find all the possible solution of the problem and use one you
+        // want
+
+    }
+
+}
