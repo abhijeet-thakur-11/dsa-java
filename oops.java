241bba3a1e85968ec00c58d9ead5b14cb4b16ee2
abhijeet <abhijeetthakur.nov2006@gmail.com>
2026-06-26 00:42:45 +0530

    Update oops example with student class

--- oops.java
+++ oops.java
@@ -10,9 +10,23 @@ class pen{
         System.out.println(this.color);
     }
 }
+class student{
+    String name;
+    int id;
+
+    public void printname(){
+        System.out.println(this.name);
+
+    }public void printid(){
+        System.out.println(this.id);
+    }
+
+
+}
 
 public class oops {
     public static void main(String[] args) {
+       /* 
         pen pen1=new pen();
         pen1.color = "Blue";
         pen1.type= "ball";
@@ -23,8 +37,21 @@ public class oops {
 
        pen1.printcolor();
        pen2.printcolor();
+       */
        
 
+       student s1 = new student();
+       s1.name ="Rohit";
+       s1.id= 1;
+       student s2 = new student();
+       s2.name = "Erick";
+       s2.id = 2;
+
+       s1.printname();
+       s1.printid();
+       s2.printname();    
+       s2.printid();
+       
 
 }
 }
