61e1ce5127a4df2d850e91400f4b9e234508c258
abhijeet <abhijeetthakur.nov2006@gmail.com>
2026-06-26 22:59:42 +0530

    Auto-update from VS Code

--- oops.java
+++ oops.java
@@ -1,4 +1,68 @@
+class pen{
+    String color ; 
+    String type ;
 
+    public void write(){
+        System.out.println("writing something ");
+
+    }
+    public void printcolor(){
+        System.out.println(this.color);
+    }
+}
+class student{
+    String name;
+    int id;
+
+    public void printinfo(String name ){
+        System.out.println(name);
+    }
+    public void printifo(int id){
+        System.out.print(id);
+    }public void printinfo(String name , int id){
+        System.out.println(name+" "+id);
+    }
+
+
+    /* 
+    //constructors 
+    student(){
+        System.out.println("constructor called");
+    }
+    // parameterize constructors 
+    student(String name , int id){
+        this.name = name;
+        this.id= id;
+    }
+    // copy comstructor 
+    student(student s2){
+        this.name = s2.name;
+        this.id=s2.id;
+
+    }*/
+
+}
+
+class shape {
+    String color;
+    public void area (){
+        System.out.println("Display area");
+    }
+}
+class tringle extends shape {   
+    public void area( int l , int h){
+        System.out.println(0.5*l*h);
+    }
+}class circle extends shape{
+    public void area(int r){
+        System.out.println(3.14*r*r);
+    }
+}
+class equilateral extends tringle{
+    public void are(int l , int h ){
+        System.out.println(0.5*l*h);
+    }
+}
 
 public class oops {
     public static void main(String[] args) {
