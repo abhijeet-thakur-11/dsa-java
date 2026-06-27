0087f934138917721e840cd7b60df1695e781062
abhijeet <abhijeetthakur.nov2006@gmail.com>
2026-06-27 06:29:46 +0530

    Add arraylist.java and update launch configuration

---
+++ arraylist.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class arraylist{
+
+
+    public static void main(String[] args){
+        ArrayList <Integer> list1 = new ArrayList<Integer>();
+        
+
+        /* 
+        // add elements 
+        list1.add(0);
+        list1.add(1);
+        list1.add(3);
+        System.out.println(list1);
+
+        // get elements
+        System.out.println(list1.get(0));
+        int element = list1.get(1);
+        System.out.println(element);
+
+        // add element in betweeen
+        list1.add(1,6);
+        System.out.println(list1);
+
+        //ste element 
+        list1.set(0,7);
+        System.out.println(list1);
+
+        //delete element 
+        list1.remove(3);
+        System.out.println(list1);
+
+        //size 
+        int size = list1.size();
+        System.out.println(size);
+
+        //iterating
+        for (int i = 0 ; i<size ; i++){
+            System.out.println(list1.get(i));
+            
+        }
+
+        //sorting 
+        Collections.sort(list1);
+        System.out.println(list1);
+        */
+        
+
+        
+    }
+}
+ 
+
